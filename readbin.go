777dce3ebb98a783ff4af15c52667231495063ed
IngenieroRicardo <138132387+IngenieroRicardo@users.noreply.github.com>
2023-11-17 10:52:49 -0600

    Add files via upload

---
+++ readbin.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "os"
+        "unicode"
+)
+
+func main(){
+    if len(os.Args) == 1 {
+        return
+    }
+    fmt.Print("\nINICIA EL ANALISIS...\n\n");
+    file, err := os.Open(os.Args[1])
+    if err != nil {
+        log.Fatal(err)
+    }
+    fi, err := file.Stat()
+    if err != nil {
+        log.Fatal(err)
+    }
+    data := make([]byte, fi.Size())
+    count, err := file.Read(data)
+    if err != nil {
+        log.Fatal(err)
+    }
+    for i := 0; i < count; i++ {
+        fmt.Print("Bin[",i,"]=",fmt.Sprintf("%08b", data[i]))
+        fmt.Print("  Hex[",i,"]=",fmt.Sprintf("0x%02x", data[i]))
+        fmt.Print("  ASCII[",i,"]=",fmt.Sprintf("%v", data[i]))
+        fmt.Print("\tChar[",i,"]=",fmt.Sprintf("%c", data[i]))
+        fmt.Println(" ")
+    }
+    fmt.Print("\n\nSTRING: \n\n");
+    for i := 0; i < count; i++ {
+
+        if ( data[i] != 0 ){
+            if unicode.IsDigit(rune(data[i])) || unicode.IsLetter(rune(data[i])) || unicode.IsSymbol(rune(data[i]))  {
+                fmt.Printf("%c", data[i])
+            } else {
+                fmt.Print(" ")
+            }
+        }
+    }
+    file.Close()
+    fmt.Print("\n\nFIN DEL ANALISIS...\n\n");
+}
+
